5eb3399691d4eeb1945e860929d76501c9ceba88
Michel Meyer <meyer_michel@msn.com>
2019-11-29 13:37:31 +0100

    conanfile  packaging

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,14 @@
 from conans import ConanFile, CMake
 
 class Calculator(ConanFile):
+    name = "Calculator"
+    version = "0.1"
+    url = "Basic-Calculator"
+    description = "<Description of Hello here>"
     settings = "os", "compiler", "build_type", "arch"
     requires = "doctest/2.3.4@bincrafters/stable" # comma-separated list of requirements
     generators = "cmake", "gcc", "txt"
+    exports_sources = "src/*", "test/*", "CMakeLists.txt"
 
     def configure_cmake(self):
         cmake = CMake(self)
@@ -11,7 +16,7 @@ class Calculator(ConanFile):
         # put definitions here so that they are re-used in cmake between
         # build() and package()
         cmake.definitions["BUILD_UTEST"] = "ON"
-        cmake.definitions["USE_COVERAGE"] = "ON"
+        cmake.definitions["USE_COVERAGE"] = "OFF"
 
         cmake.configure()
         return cmake
@@ -20,7 +25,12 @@ class Calculator(ConanFile):
         cmake = CMake(self)
         cmake = self.configure_cmake()
         cmake.build()
-        # run unit tests after the build
         cmake.test()
-        self.run("../show_coverage.sh")
-        
+        #self.run("../show_coverage.sh")
+
+    def package(self):
+        self.copy("*.hpp", dst="include", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["Calculator"]
